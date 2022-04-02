9aea61ab116533f65611e324a64f6be06311fdbb
daniel.andersen <daniel.andersen@unity3d.com>
2022-04-02 16:34:33 +0200

    Remove renderfeature and vector ui

--- Assets/Scripts/Systems/GameManager.cs
+++ Assets/Scripts/Systems/GameManager.cs
@@ -37,9 +37,6 @@ namespace Systems
 
         protected override void OnDestroy() => m_ResourceTypeToString.Dispose();
         
-        Material m_Material;
-        RenderTexture m_Texture;
-        float2 m_CameraResolution;
         protected override void OnStartRunning()
         {
             var docEntity = GetSingletonEntity<UIDocument>();
@@ -54,41 +51,10 @@ namespace Systems
                 button.AddToClassList("jump");
             }
             m_Label = doc.rootVisualElement.Q<Label>();
-
-            // Setup Drawing
-            var cam = Camera.main;
-            m_CameraResolution = new int2(cam.pixelWidth, cam.pixelHeight);
-            m_Texture = new RenderTexture((int) m_CameraResolution.x, (int) m_CameraResolution.y,0);
-            m_Material = new Material(Shader.Find("Unlit/Vector"));
-            var overlayStyle = doc.rootVisualElement.Q<VisualElement>("VectorOverlay").style;
-            overlayStyle.backgroundImage = new StyleBackground {value = new Background {renderTexture = m_Texture}};
-            
-            // Create Scene
-            m_PathProp = new PathProperties {Stroke = new Stroke {Color = Color.red, HalfThickness = 5f}};
         }
         
-
-        PathProperties m_PathProp;
         protected override void OnUpdate()
-        {
-            // Create sprite and draw it.
-            var circle = new Shape
-            {
-                PathProps = m_PathProp
-            };
-            VectorUtils.MakeCircleShape(circle, m_CameraResolution*0.5f, (float)(((math.sin(Time.ElapsedTime)*0.5+0.5)*0.9+0.1)*500));
-            var scene = new Scene{Root = new SceneNode{Shapes = new List<Shape> {circle}}};
-            var geo = VectorUtils.TessellateScene(scene, new VectorUtils.TessellationOptions
-            {
-                SamplingStepSize = 50f,
-                StepDistance = 10f,
-            });
-            var sprite = VectorUtils.BuildSprite(geo, new Rect(0,0,m_CameraResolution.x,m_CameraResolution.y),1f, VectorUtils.Alignment.Center, Vector2.zero, 64);
-            var oldRT = RenderTexture.active;
-            RenderTexture.active = m_Texture;
-            VectorUtils.RenderSprite(sprite, m_Material);
-            RenderTexture.active = oldRT;
-            
+        { 
             if (m_ButtonClicked)
             {
                 Entities.ForEach((ref PhysicsVelocity vel, in PhysicsMass mass) 
