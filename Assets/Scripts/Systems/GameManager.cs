fb28abaf157a6d60357260c11a95c51def284d70
daniel.andersen <daniel.andersen@unity3d.com>
2022-04-02 12:37:11 +0200

    Vectors now work in UI

--- Assets/Scripts/Systems/GameManager.cs
+++ Assets/Scripts/Systems/GameManager.cs
@@ -12,6 +12,7 @@ using Unity.Rendering;
 using Unity.Transforms;
 using Unity.VectorGraphics;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 using Material = UnityEngine.Material;
 
@@ -36,13 +37,15 @@ namespace Systems
 
         protected override void OnDestroy() => m_ResourceTypeToString.Dispose();
         
-        Sprite m_Sprite;
         Material m_Material;
+        RenderTexture m_Texture;
+        float2 m_CameraResolution;
         protected override void OnStartRunning()
         {
             var docEntity = GetSingletonEntity<UIDocument>();
             var doc = EntityManager.GetComponentObject<UIDocument>(docEntity);
 
+            // Setup UI
             var button = doc.rootVisualElement.Q<Button>("Jump");
             if (button!=null)
             {
@@ -50,32 +53,41 @@ namespace Systems
                 button.clicked += () => m_ButtonClicked = true;
                 button.AddToClassList("jump");
             }
-
             m_Label = doc.rootVisualElement.Q<Label>();
 
-            var renderer = EntityManager.GetComponentObject<SpriteRenderer>(docEntity);
-            //var background = doc.rootVisualElement.Q<VisualElement>("VectorOverlay").style.backgroundImage.value;
-            var circle = new Shape();
-            var pathProp = new PathProperties {Stroke = new Stroke {Color = Color.red, HalfThickness = 0.01f}};
-            circle.PathProps = pathProp;
-            VectorUtils.MakeCircleShape(circle, Vector2.zero, 1f);
-            var scene = new Scene{Root = new SceneNode{Shapes = new List<Shape> {circle}}};
-            var options = new VectorUtils.TessellationOptions
-            {
-                SamplingStepSize = 0.1f,
-                StepDistance = 0.01f,
-                
-            };
-            var geo = VectorUtils.TessellateScene(scene, options);
-            m_Sprite = VectorUtils.BuildSprite(geo, 1, VectorUtils.Alignment.BottomLeft, Vector2.zero, 64);
+            // Setup Drawing
+            var cam = Camera.main;
+            m_CameraResolution = new int2(cam.pixelWidth, cam.pixelHeight);
+            m_Texture = new RenderTexture((int) m_CameraResolution.x, (int) m_CameraResolution.y,0);
             m_Material = new Material(Shader.Find("Unlit/Vector"));
-            renderer.sprite = m_Sprite;
+            var overlayStyle = doc.rootVisualElement.Q<VisualElement>("VectorOverlay").style;
+            overlayStyle.backgroundImage = new StyleBackground {value = new Background {renderTexture = m_Texture}};
+            
+            // Create Scene
+            m_PathProp = new PathProperties {Stroke = new Stroke {Color = Color.red, HalfThickness = 5f}};
         }
         
 
+        PathProperties m_PathProp;
         protected override void OnUpdate()
         {
-            VectorUtils.RenderSprite(m_Sprite, m_Material);
+            // Create sprite and draw it.
+            var circle = new Shape
+            {
+                PathProps = m_PathProp
+            };
+            VectorUtils.MakeCircleShape(circle, m_CameraResolution*0.5f, (float)(((math.sin(Time.ElapsedTime)*0.5+0.5)*0.9+0.1)*500));
+            var scene = new Scene{Root = new SceneNode{Shapes = new List<Shape> {circle}}};
+            var geo = VectorUtils.TessellateScene(scene, new VectorUtils.TessellationOptions
+            {
+                SamplingStepSize = 50f,
+                StepDistance = 10f,
+            });
+            var sprite = VectorUtils.BuildSprite(geo, new Rect(0,0,m_CameraResolution.x,m_CameraResolution.y),1f, VectorUtils.Alignment.Center, Vector2.zero, 64);
+            var oldRT = RenderTexture.active;
+            RenderTexture.active = m_Texture;
+            VectorUtils.RenderSprite(sprite, m_Material);
+            RenderTexture.active = oldRT;
             
             if (m_ButtonClicked)
             {
